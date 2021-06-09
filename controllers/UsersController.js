179e9f4e2e851908dfdc2998e215f899e03e4ae7
lancewburklin <1677@holbertonschool.com>
2021-06-09 15:07:31 -0500

    Adding user creation

---
+++ controllers/UsersController.js
@@ -0,0 +1,25 @@
+import dbClient from '../utils/db';
+
+const crypto = require('crypto');
+
+export default class UsersController {
+  static async postNew(req, res) {
+    const { email } = req.body;
+    const { password } = req.body;
+    if (email === undefined) {
+      return res.status(400).send({ error: 'Missing email' });
+    }
+    if (password === undefined) {
+      return res.status(400).send({ error: 'Missing password' });
+    }
+    const info = await dbClient.db.collection('users').find({ email }).toArray();
+    if (info.length !== 0) {
+      dbClient.db.collection('users').deleteOne({ email });
+      return res.status(400).send({ error: 'Already exist' });
+    }
+    let hasPass = crypto.createHash('sha1');
+    hasPass = hasPass.update(password, 'utf-8').digest('hex');
+    const newItem = await dbClient.db.collection('users').insertOne({ email, password: hasPass });
+    return res.status(200).send({ id: newItem.ops[0]._id, email });
+  }
+}
